cc36286229683809c62eb24b39222bd29e144524
Danny Katoro <danielkatoro@gmail.com>
2022-03-21 20:44:25 +0100

    creating product seeder

---
+++ database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i=0; $i < 30; $i++) { 
+            # code...
+        }
+    }
+}
