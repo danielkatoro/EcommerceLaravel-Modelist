7510fd572c7334157ebaa73911acc3e5d32447a6
Danny Katoro <danielkatoro@gmail.com>
2022-03-25 10:01:54 +0100

    payement method with stripe

--- app/Http/Controllers/ChekoutController.php
+++ app/Http/Controllers/ChekoutController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Stripe\Stripe;
 use Illuminate\Http\Request;
+use Stripe\Checkout\Session;
 
 class ChekoutController extends Controller
 {
@@ -13,6 +15,23 @@ class ChekoutController extends Controller
      */
     public function index()
     {
+        Stripe::setApiKey('sk_test_51Kg9rDFWBnanu8VzzrCjF8iKjrwVzlc6za536BQMhfDxnR3w8iQIk0pOctf5WeY6UJiGlqmVqhsGmlipNJNc3QF700P1LXCzvc');
+
+        $session = Session::create([
+            'line_items' => [[
+              'price_data' => [
+                'currency' => 'usd',
+                'product_data' => [
+                  'name' => 'T-shirt',
+                ],
+                'unit_amount' => 2000,
+              ],
+              'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            'success_url' => 'https://example.com/success',
+            'cancel_url' => 'https://example.com/cancel',
+          ]);
         return view('checkout.index');
     }
 
